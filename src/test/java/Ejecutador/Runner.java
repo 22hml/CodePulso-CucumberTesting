f70917f5f63e47f5347a44d5a7c78ecb5f4f24a5
22hml <helm.meneses@duocuc.cl>
2024-11-22 20:21:30 -0300

    avances en reportes cucumber
    avances en reportes cucumber

--- src/test/java/Ejecutador/Runner.java
+++ src/test/java/Ejecutador/Runner.java
@@ -1,14 +1,19 @@
 package Ejecutador;
 
 import org.junit.runner.RunWith;
-import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-    features = "src/test/resources/Feature",
-    glue = "DefinicionPasos",
-    plugin = {"pretty", "html:target/cucumber-reports.html"},
+    features = {"src/test/resources/Feature"},
+    glue = {"DefinicionPasos"},
+    plugin = { 
+        "pretty", 
+        "json:target/cucumber-reports/cucumber.json",
+        "html:target/cucumber-reports/report.html",
+        "junit:target/cucumber-results.xml" 
+    },
     monochrome = true
 )
 public class Runner {
